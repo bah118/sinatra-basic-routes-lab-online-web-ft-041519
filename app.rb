ae45d6d4942b45eeb5de40af2f6a114f91fddd1c
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-04 18:36:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Beth Ann"
+  end
+
+  get '/hometown' do 
+    "My hometown is Wilkes-Barre, Pennsylvania"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Ghostflowers"
+  end
+  
 end
